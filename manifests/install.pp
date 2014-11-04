4499406d6a93c5e2dc0289551d29a7cfa09f043b
Magno da Costa <magnocosta@MacBook-Pro-de-Magno.local>
2014-11-04 21:15:08 -0200

    Bugfix ao instalar Redis.

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,20 +3,22 @@
 
 class redis::install {
 
+
+    file { "/opt/redis-stable":
+        ensure => "directory",
+        alias => "create-redis-directory"
+     }
+
      exec { "download-redis":
         command => "/usr/bin/wget http://download.redis.io/redis-stable.tar.gz",
-        unless  => "/bin/ls | /bin/grep redis-stable.tar.gz"
+        unless  => "/bin/ls | /bin/grep redis-stable.tar.gz",
+        require => "create-redis-directory"
      }
 
-     exec { "tar-redis-stable":
-        command => "/bin/tar xvzf redis-stable.tar.gz",
-        creates => "redis-stable",
-        require => Exec["download-redis"]
-     }
-
-     exec { "make-redis":
-        command => "cd redis-stable && /usr/bin/make",
-        require => Exec["tar-redis-stable"]
-     }
+    # exec { "tar-redis-stable":
+    #    command => "/bin/tar xvzf redis-stable.tar.gz -C /opt/redis-stable",
+    #    creates => "/opt/redis-stable",
+    #    require => Exec["download-redis"]
+    # }
 
 }
