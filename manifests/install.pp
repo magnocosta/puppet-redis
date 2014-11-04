102e928746c95085923c8b2a10f0ffbe134dba6d
Magno da Costa <magnocosta@MacBook-Pro-de-Magno.local>
2014-11-04 17:42:59 -0200

    Adicionado os scripts de instalacao de Redis.

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,6 +3,20 @@
 
 class redis::install {
 
-    
+     exec { "download-redis":
+        command => "/usr/bin/wget http://download.redis.io/redis-stable.tar.gz",
+        unless  => "/bin/ls | /bin/grep redis-stable.tar.gz"
+     }
+
+     exec { "tar-redis-stable":
+        command => "/bin/tar xvzf redis-stable.tar.gz",
+        creates => "redis-stable",
+        require => Exec["download-redis"]
+     }
+
+     exec { "make-redis":
+        command => "cd redis-stable && /usr/bin/make",
+        require => Exec["tar-redis-stable"]
+     }
 
 }
